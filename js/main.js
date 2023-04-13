054137e4b092d768f69da6fa8930233e31a48f1e
Solatido143 <79861219+Solatido143@users.noreply.github.com>
2023-04-13 22:41:39 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,12 +2,18 @@ function submithandler() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     
-    if (username === "" || password === "") {
+    if (username == "" && password == "") {
         document.getElementById("error-message").innerHTML = "Please enter a username and password.";
-        alert("Invalid input");
+        return false;
+    } else if (username === "") {
+        document.getElementById("error-message").innerHTML = "Please enter a username.";
+        return false;
+    } else if (password === "") {
+        document.getElementById("error-message").innerHTML = "Please enter a password";
         return false;
     } else {
         document.getElementById("error-message").innerHTML = "";
+        document.getElementById('success-message').innerHTML = "Logging In";
         return true;
     }
 }
