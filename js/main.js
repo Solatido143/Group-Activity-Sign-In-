92f2a48b168da7038fc5946028673f4d6e140baf
Solatido143 <79861219+Solatido143@users.noreply.github.com>
2023-04-13 21:38:42 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
 function submithandler() {
-    // Get the values of the username and password inputs
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     
-    // Check if the username and password inputs are empty
     if (username === "" || password === "") {
-      // If either input is empty, display an error message and prevent the form from submitting
         document.getElementById("error-message").innerHTML = "Please enter a username and password.";
         alert("Invalid input");
         return false;
     } else {
-      // If both inputs have a value, clear any existing error message and allow the form to submit
         document.getElementById("error-message").innerHTML = "";
         return true;
     }
